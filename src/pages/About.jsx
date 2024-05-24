d8f56a7a5ed2d0c76d49cdb4d8ce00a64c82ba1c
Bobby Caya <bob.caya1@gmail.com>
2024-05-24 14:17:43 -0600

    styling nav and header

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function About(){
-  return ( <h2>About Page component</h2>)
+  return ( 
+    <div className="about-page-container">
+      <p>image</p>
+      <div className="about-page-content">
+        <h1>Go on a VanVenture!</h1>
+        <p>At VanVentures, we believe in the power of travel to inspire and rejuvenate. Our mission is to provide you with the perfect vehicle for your journey, combining comfort, reliability, and adventure. Each of our vans is designed with you in mind, featuring all the amenities you need to make your trip unforgettable. From the mountains to the sea, our vans are your home on wheels.</p>
+      </div>
+    </div>
+  )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ export default function Home(){
     <div className="home-container">
       <h1>Welcome to VanVentures!</h1>
       <p>Discover the freedom of the open road with VanVentures, your premier van rental service. Whether you’re planning a weekend getaway or a cross-country adventure, our fleet of fully equipped vans is ready to take you wherever your heart desires.</p>
+      <Link to="vans">Find your van!</Link>
     </div>
   )
 }
