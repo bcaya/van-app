31b779d2ee487353a46e2173729e92a3e08c1bc8
Bobby Caya <bob.caya1@gmail.com>
2024-05-24 21:20:00 -0600

    add about image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+const imgUrl = "src/assets/about-image.jpg"
 
 export default function About(){
   return ( 
     <div className="about-page-container">
-      <p>image</p>
+      <img src={imgUrl} />
       <div className="about-page-content">
         <h1>Go on a VanVenture!</h1>
         <p>At VanVentures, we believe in the power of travel to inspire and rejuvenate. Our mission is to provide you with the perfect vehicle for your journey, combining comfort, reliability, and adventure. Each of our vans is designed with you in mind, featuring all the amenities you need to make your trip unforgettable. From the mountains to the sea, our vans are your home on wheels.</p>
