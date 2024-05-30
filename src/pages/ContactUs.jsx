1ca7bfa836585f3be3d1a06e2d872fad80360362
Bobby Caya <bob.caya1@gmail.com>
2024-05-29 20:50:23 -0600

    styling fixes

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
-import {Box, Stack, Center} from '../assets/CSS/StylesMain'
+import {Box, Stack, Center, Cluster, Sidebar, Frame} from '../assets/CSS/StylesMain'
 
 export default function ContactUs(){
   return(
     <box-l className="contact-container">
-      <center-l className="contact-header">
+      <sidebar-l side="right" content-min="66.6%" noStretch>
+        <frame-l ratio="4:5">
+        <img src='src/assets/contact-us-sm.jpg'></img>
+        </frame-l>
+
+      <box-l className="contact-header">
         <stack-l>
-        <h2 className="contact-h2">Contact Us</h2>
-      
-      <p>Have questions or need assistance? Our friendly team is here to help.</p>
-      <ul>
-      <li>**Email**: support@vanventures.com</li> 
-      <li>**Phone**: 1-800-VANVENTURE</li> 
-      <li>**Address**: 123 Adventure Road, Sunnyvale, CA</li>  
-      </ul>
-        <p>Follow us on social media for travel inspiration and updates.</p>
+          <h1 className="contact-h2">Contact Us</h1>
+        <p>Have questions or need assistance? Our friendly team is here to help.</p>
         <ul>
-      <li>**Instagram**: @VanVentures</li> 
-      <li>**Facebook**: /VanVentures</li> 
-      <li>**Twitter**: @VanVentures</li>  
-      </ul>
-      
+          <stack-l>
+            <li>Email: support@vanventures.com</li> 
+            <li>Phone: 1-800-VANVENTURE</li> 
+            <li>Address: 123 Adventure Road, Sunnyvale, CA</li>  
+          </stack-l>
+        </ul>
+      <h2>Follow us on social media for travel inspiration and updates.</h2>
+          <ul>
+            <stack-l>
+              <li>Instagram: @VanVentures</li> 
+              <li>Facebook: /VanVentures</li> 
+              <li>Twitter: @VanVentures</li>  
+            </stack-l>
+          </ul>
         </stack-l>
-       
-      </center-l>
+      </box-l>
+      </sidebar-l>
+
     </box-l>
   )
 }
