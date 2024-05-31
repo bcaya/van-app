17216949ff2b1d79101ca64548c5f78a129254b6
Bobby Caya <bob.caya1@gmail.com>
2024-05-30 20:42:50 -0600

    fix styling

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -4,30 +4,25 @@ import {Box, Stack, Center, Cluster, Sidebar, Frame} from '../assets/CSS/StylesM
 export default function ContactUs(){
   return(
     <box-l className="contact-container">
-      <sidebar-l side="left" content-min="20%" noStretch>
+      <sidebar-l side="right" content-min="50%" >
       <box-l>
-      <div>
-
-        <img src='src/assets/contact-us-sm.jpg'></img>
-      
-      </div>
-
-      </box-l>  
-      <box-l className="contact-header">
-        <stack-l>
-          <h1 className="contact-h2">Contact Us</h1>
+      <h1 className="contact-h2">Contact Us</h1>
         <p>Have questions or need assistance? Our friendly team is here to help.</p>     
-            <div role="list">
+            <stack-l role="list">
             <div role="listitem">Email: support@vanventures.com</div> 
             <div role="listitem">Phone: 1-800-VANVENTURE</div> 
             <div role="listitem">Address: 123 Adventure Road, Sunnyvale, CA</div>  
-            </div>
+            </stack-l>
+      </box-l>  
+      <box-l className="contact-header">
+        <stack-l>
+
       <h2>Follow us on social media for travel inspiration and updates.</h2>
-            <div role="list">
+            <stack-l role="list">
               <div role="listitem">Instagram: @VanVentures</div> 
               <div role="listitem">Facebook: /VanVentures</div> 
               <div role="listitem">Twitter: @VanVentures</div>  
-            </div>
+            </stack-l>
         </stack-l>
       </box-l>
       </sidebar-l>
